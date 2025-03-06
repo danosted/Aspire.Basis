0498bfceea3290e28795a98ebfb3cb46fc9d0a96
Dan Rosenberg Osted <danosted@gmail.com>
2025-03-06 15:01:21 +0100

    added some client packages. added todo file

---
+++ Web/Web.Client/todos.md
@@ -0,0 +1,7 @@
+# TODO:
+Add Tailwind Variants
+[TailwindCSS Variants](https://www.tailwind-variants.org/docs/introduction)
+
+
+Add ShadCN
+[ShadCN](https://ui.shadcn.com/docs/installation/vite)
--- Web/Web.Client/vite.config.ts
+++ Web/Web.Client/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({}), react(), mkcert()],
+  plugins: [
+    TanStackRouterVite({}),
+    react(),
+    mkcert(),
+    tailwindcss()
+  ],
 })
